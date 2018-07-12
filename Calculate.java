db5cee5c5972367c12d50bbb61891448a58f8fae
Kecher <k.v.kacharha@gmail.com>
2018-07-13 00:35:40 +0300

    Calculete

---
+++ Calculate.java
@@ -0,0 +1,35 @@
+public class Calculate{
+  
+	public static void main(String[] arg){
+
+
+		System.out.println("Calculate...");
+		double x = 23;
+		double y = 11; 
+		double summ = x - y;
+		System.out.println("Sum " + summ);
+		
+		System.out.println("Calculate...");
+		double x1 = 26, y1 = 23;
+		double summ1 = x1 * y1;
+		System.out.println("Sum " + summ);
+
+		System.out.println("Calculate...");
+		double x2 = 110, y2 = 55;
+		double summ2 = x2 / y2;
+		System.out.println("Sum " + summ);
+
+		System.out.println("Calculate...");
+		double x3 = 23;
+		double y3 = 2;
+		System.out.println("Sum " + Math.pow(x3, y3));
+		
+
+		
+
+
+		
+
+	}
+
+}
